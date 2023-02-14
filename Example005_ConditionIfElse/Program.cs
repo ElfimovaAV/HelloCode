96e9b3dd7270feff8f90019579af72451b3bfacf
Anastasia <nastya.elfimova@inbox.ru>
2023-02-15 02:52:37 +0500

    Add recursia

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if (Console.ReadLine()!.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
 else
 {
     Console.Write("Привет,");
-    Console.WriteLine(username);
+    Console.WriteLine(Console.ReadLine()!);
 }
